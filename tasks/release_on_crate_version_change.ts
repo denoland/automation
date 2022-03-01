84d6dadb43e3970493f0cedebed84d3865d06b9b
David Sherret <dsherret@users.noreply.github.com>
2022-03-01 14:10:23 -0500

    feat: add `--publish` flag to release_on_crate_version_change.ts (#3)

--- tasks/release_on_crate_version_change.ts
+++ tasks/release_on_crate_version_change.ts
@@ -11,6 +11,21 @@
 // Note: this will detect whether to add a `v` prefix or not based on the most recent
 // version tag.
 //
+// # CLI Arguments
+//
+// If you have multiple crates, then you will need to provide the crate name
+// to use to determine the tag name as an argument to the script.
+//
+// ```bash
+// deno run -A --no-check <url-to-this-module> <crate-name-goes-here>
+// ```
+//
+// If you want to publish to crates.io, provide a `--publish` flag:
+//
+// ```bash
+// deno run -A --no-check <url-to-this-module> --publish
+// ```
+//
 // # Example Use
 //
 // ```yml
@@ -22,21 +37,21 @@
 //     github.ref == 'refs/heads/main'
 //    run: deno run -A --no-check <url-to-this-module>
 // ```
-//
-// If you have multiple crates, then you will need to provide the crate name
-// to use to determine the tag name as an argument to the script.
-//
-// ```bash
-// deno run -A --no-check <url-to-this-module> <crate-name-goes-here>
-// ```
 
-import { path, Repo } from "../mod.ts";
+import { containsVersion, path, Repo } from "../mod.ts";
 import { createOctoKit, getGitHubRepository } from "../github_actions.ts";
 
+const cliArgs = getCliArgs();
 const cwd = path.resolve(".");
 const repoName = path.basename(cwd);
 const repo = await Repo.load(repoName, cwd);
 const octokit = createOctoKit();
+
+// only run this for commits that contain a version number in the commit message
+if (!containsVersion(await repo.gitCurrentCommitMessage())) {
+  console.log("Exiting: No version found in commit name.");
+  Deno.exit();
+}
 
 // safeguard for in case if someone doesn't run this on the main branch
 await repo.assertCurrentBranch("main");
@@ -49,6 +64,13 @@ const tagName = repoTags.getTagNameForVersion(mainCrate.version);
 if (repoTags.has(tagName)) {
   console.log(`Tag ${tagName} already exists.`);
 } else {
+  if (cliArgs.publish) {
+    console.log(`Publishing ${tagName}...`);
+    for (const crate of repo.getCratesPublishOrder()) {
+      await crate.publish();
+    }
+  }
+
   console.log(`Tagging ${tagName}...`);
   await repo.gitTag(tagName);
   await repo.gitPush("origin", tagName);
@@ -68,11 +90,30 @@ if (repoTags.has(tagName)) {
 function getMainCrate() {
   if (repo.crates.length === 1) {
     return repo.crates[0];
-  } else if (Deno.args.length >= 1) {
-    return repo.getCrate(Deno.args[0]);
+  } else if (cliArgs.crate != null) {
+    return repo.getCrate(cliArgs.crate);
   } else {
     throw new Error(
       `You must supply a crate name CLI argument.\n${repo.crateNamesText()}`,
     );
   }
+}
+
+function getCliArgs() {
+  // very basic arg parsing... should improve later
+  let crate: string | undefined;
+  let publish = false;
+  for (const arg of Deno.args) {
+    if (arg === "--publish") {
+      publish = true;
+    } else if (crate == null) {
+      crate = arg;
+    } else {
+      throw new Error(`Invalid arguments: ${Deno.args.join(" ")}`);
+    }
+  }
+  return {
+    publish,
+    crate,
+  };
 }
