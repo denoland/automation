250cd1a872ac0dfed51dcd9034302d5cf380394d
David Sherret <dsherret@gmail.com>
2023-03-14 18:32:48 -0400

    Update

--- deps.ts
+++ deps.ts
@@ -1,8 +1,5 @@
 // Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
 
 export * as semver from "https://deno.land/std@0.179.0/semver/mod.ts";
-export {
-  default as $,
-  PathRef,
-} from "https://raw.githubusercontent.com/dsherret/dax/9347044a24f00487918ca30138de9f740ff69efc/mod.ts";
-export * as dax from "https://raw.githubusercontent.com/dsherret/dax/9347044a24f00487918ca30138de9f740ff69efc/mod.ts";
+export { default as $, PathRef } from "https://deno.land/x/dax@0.30.0/mod.ts";
+export * as dax from "https://deno.land/x/dax@0.30.0/mod.ts";
